4b1358d6ac4418c4dc6c89b49c467af3879302ff
Ilya Valeev <iljavaleev@gmail.com>
2022-10-08 21:36:10 +0500

    add

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'PAGE_SIZE': 6,
